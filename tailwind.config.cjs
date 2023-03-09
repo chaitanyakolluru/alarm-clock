429cd82e76acc8da1456b52b80d2cd89b64b61f1
Chaitanya Kolluru <chaitanya.r.kolluru@gmail.com>
2023-03-09 12:56:50 -0600

    cleaning up tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,13 +6,6 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    styled: true,
     themes: ['night', 'light', 'dark'],
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: '',
-    darkTheme: 'night',
   },
 };
